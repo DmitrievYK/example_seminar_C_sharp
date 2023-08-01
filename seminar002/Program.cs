b97949733bb3a1b415f88bd29e1864c3ac0877a8
DmitrievYK <fireworcks@gmail.com>
2023-08-01 21:29:52 +0300

    Две решеные задачи на семенаре 2

---
+++ seminar002/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача №1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+//a = 25, b = 5 -> да 
+//a = 2, b = 10 -> нет 
+//a = 9, b = -3 -> да 
+//a = -3 b = 9 -> нет
+
+//Задача №3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+//3 -> Среда 
+//5 -> Пятница
+Console.WriteLine("Введите первое число ");
+string input1 = Console.ReadLine();
+Console.WriteLine("Введите второе число ");
+string input2 = Console.ReadLine();
+int number1 = Convert.ToInt32(input1);
+int number2 = Convert.ToInt32(input2);
+
+int result_number2 = number2 * number2;
+
+if (number1 == result_number2)
+{
+    Console.WriteLine("Да, является ");
+}
+else
+{
+    Console.WriteLine("Нет, является ");
+}
---
+++ seminar003/Program.cs
@@ -0,0 +1,40 @@
+﻿//адача №3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+//3 -> Среда 
+//5 -> Пятница
+
+Console.WriteLine("Введите число от 1 до 7 ");
+string input = Console.ReadLine();
+int number = Convert.ToInt32(input);
+
+if (number == 1)
+{
+    Console.WriteLine("понедельник");
+}
+if (number == 2)
+{
+    Console.WriteLine("вторник");
+}
+if (number == 3)
+{
+    Console.WriteLine("среда");
+}
+if (number == 4)
+{
+    Console.WriteLine("четвер");
+}
+if (number == 5)
+{
+    Console.WriteLine("пятница");
+}
+if (number == 6)
+{
+    Console.WriteLine("суббота");
+}
+if (number == 7)
+{
+    Console.WriteLine("воскресенье");
+}
+if (number > 7 && number < 1)
+{
+    Console.WriteLine("Введите значение от 1 до 7");
+}

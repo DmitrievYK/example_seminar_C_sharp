e4d692c02e90827e98a3d99d037ea8d917ff9938
DmitrievYK <fireworcks@gmail.com>
2023-08-01 21:55:10 +0300

    Трейтий вариант решения последней задачи

---
+++ seminar005/Program.cs
@@ -0,0 +1,37 @@
+﻿// Третий вариант решения последней задачи
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+switch(number)
+{
+    case 1:
+    Console.WriteLine("1");
+    break;
+
+    case 2:
+    Console.WriteLine("2");
+    break;
+
+    case 3:
+    Console.WriteLine("3");
+    break;
+
+    case 4:
+    Console.WriteLine("4");
+    break;
+
+    case 5:
+    Console.WriteLine("5");
+    break;
+
+    case 6:
+    Console.WriteLine("6");
+    break;
+
+    case 7:
+    Console.WriteLine("7");
+    break;
+
+    default:
+    Console.WriteLine("0");
+    break;
+}

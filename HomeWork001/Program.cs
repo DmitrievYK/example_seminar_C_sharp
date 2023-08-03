684e36804db329961889fbb532a06ca57626c5d0
DmitrievYK <fireworcks@gmail.com>
2023-08-03 20:22:38 +0300

    ДЗ задание 1 решение

---
+++ HomeWork001/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдает, 
+//какое число большее, а какое меньше
+Console.WriteLine("Введите число a =: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число b =: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+
+if (num1 > num2)
+{
+    Console.WriteLine("a = " + num1 );
+    Console.WriteLine("b = " + num2 );
+    Console.WriteLine("MAX = " + num1 );
+}
+if (num1 < num2)
+{
+    Console.WriteLine("a = " + num1 );
+    Console.WriteLine("b = " + num2 );
+    Console.WriteLine("MAX = " + num2 );
+}
+else if (num1 == num2)
+{
+    Console.WriteLine("Числа равны");
+}

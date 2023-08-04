0e9ef451b289963dd7dafac349863a0b12216a3a
DmitrievYK <fireworcks@gmail.com>
2023-08-04 21:08:47 +0300

    семинар №2 решение задачи 2 и 3

--- 2seminar002/Program.cs
+++ 2seminar002/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит случайное трехзначное число и 
+// удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+Random rand = new Random();
+int number = rand.Next(99, 1000);
+Console.WriteLine(number);
+
+int a = number / 100;
+int b = number % 10;
+
+string s1 = a.ToString();
+string s2 = b.ToString();
+string result = s1 + s2;
+Console.WriteLine(result);
+
---
+++ 2seminar003/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет,
+//  является ли одно число квадратом другого.
+// 5, 25  ->  да
+// -4, 16  ->  да
+// 25, 5  ->  да
+// 8,9  ->  нет
+
+Console.WriteLine("Введите первое число ");
+string input1 = Console.ReadLine();
+Console.WriteLine("Введите второе число ");
+string input2 = Console.ReadLine();
+int number1 = Convert.ToInt32(input1);
+int number2 = Convert.ToInt32(input2);
+
+int result_number2 = number2 * number2;
+
+if (number1 == result_number2 || number2 == number1 * number1)
+{
+    Console.WriteLine("Да, является ");
+}
+else
+{
+    Console.WriteLine("Нет, является ");
+}

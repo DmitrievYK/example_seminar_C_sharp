31ce051f263cb327b1bdbfc165d4d8ba4f833203
DmitrievYK <fireworcks@gmail.com>
2023-08-05 19:15:13 +0300

    Решение ДЗ - задача номер 6

--- HomeWork002/Program.cs
+++ HomeWork002/Program.cs
@@ -1,5 +1,8 @@
 ﻿// Напишите программу, которая принимает на вход три числа и выдает максимальное 
 // из этих чисел.
+// 2 3 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
 Console.WriteLine("Введите число a =: ");
 int a = Convert.ToInt32(Console.ReadLine());
 
@@ -11,31 +14,15 @@ int c = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine();
 
-if ((a < c) && (b < c))
+if ((a <= c) && (b <= c))
 {
     Console.WriteLine("Max = " + c);
 }
-else if ((a < b) && (c < b))
+else if ((a <= b) && (c <= b))
 {
     Console.WriteLine("Max = " + b);
 }
-else if ((a > b) && (c < a))
+else if ((a >= b) && (c <= a))
 {
     Console.WriteLine("Max = " + a);
 }
-// else if ((a == b) && (c < a))
-// {
-//     Console.WriteLine("Max = " + a);
-// }
-// else if ((a == b) && (c < b))
-// {
-//     Console.WriteLine("Max = " + b);
-// }
-// else if ((a == c) && (b < c))
-// {
-//     Console.WriteLine("Max = " + c);
-// }
-// else if ((a == b) && (a < c))
-// {
-//     Console.WriteLine("Max = " + c);
-// }

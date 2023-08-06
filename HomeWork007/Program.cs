2f8dfda3e6a83a8c9814ffc4c9eaa9347d5f38f8
DmitrievYK <fireworcks@gmail.com>
2023-08-06 18:07:20 +0300

    Второй варианти решения задачи №15

--- HomeWork007/Program.cs
+++ HomeWork007/Program.cs
@@ -9,34 +9,34 @@ int number = Convert.ToInt32(Console.ReadLine());
 switch(number)
 {
     case 1:
-    Console.WriteLine("Пн - нет");
+    Console.WriteLine("Пн - работаем");
     break;
 
     case 2:
-    Console.WriteLine("Вт - нет");
+    Console.WriteLine("Вт - работаем");
     break;
 
     case 3:
-    Console.WriteLine("Ср - нет");
+    Console.WriteLine("Ср - работаем");
     break;
 
     case 4:
-    Console.WriteLine("Чт - нет");
+    Console.WriteLine("Чт - работаем");
     break;
 
     case 5:
-    Console.WriteLine("Пт - нет");
+    Console.WriteLine("Пт - работаем");
     break;
 
     case 6:
-    Console.WriteLine("Сб - Да");
+    Console.WriteLine("Сб - Ура, отдыхаем");
     break;
 
     case 7:
-    Console.WriteLine("Вск - Да");
+    Console.WriteLine("Вск - Ура, отдыхаем");
     break;
 
     default:
-    Console.WriteLine("Введите число от 1 до 7");
+    Console.WriteLine("Нет такого дня недели");
     break;
 }

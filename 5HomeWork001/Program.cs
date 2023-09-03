733e9c9eedca2912e94d00c5ea0e454cd55d0e35
DmitrievYK <fireworcks@gmail.com>
2023-09-03 19:50:25 +0300

    Решение ДЗ №6

---
+++ 5HomeWork001/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int[] array = new int[5];
+
+int[] FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите элемент массива № {i + 1} из 5: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    Console.WriteLine("[" + String.Join(", ", array) + "]");
+    return array;
+}
+
+int HowMuchNumbersIsPositive(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+FillArray(array);
+Console.WriteLine($"Пользователь ввел {HowMuchNumbersIsPositive(array)} чисел больше нуля");
+
+
+
---
+++ 5HomeWork002/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+//  заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//  значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Программа которая находит пересечания двух прямых, по k-угловому коэффициенту и b-свободному коэффициенту");
+
+double[] pointA = new double[2];
+double[] pointB = new double[2];
+
+double[] SetFunctionNumber(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i <= 0)
+        {
+            Console.Write($"Введите угловой коэффициент k: ");
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        else
+        {
+            Console.Write($"Введите свободный коэффициент b: ");
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+    Console.WriteLine("[" + String.Join(", ", array) + "]");
+    return array;
+}
+
+void LineIntersectionCoordinates(double[] pointA, double[] pointB)
+{
+    if (pointA[0] == pointB[0] && pointA[1] == pointB[1])
+    {
+        Console.WriteLine("Прямые лежат на одной прямой");
+    }
+    else if (pointA[0] == pointB[0])
+    {
+        Console.WriteLine("Прямые параллельны");
+    }
+    else
+    {
+        double[] result = new double[2];
+        for (int i = 0; i < result.Length; i++)
+        {
+            if (i <= 0)
+            {
+                result[i] = (pointA[1] - pointB[1]) / (pointB[0] - pointA[0]); ;
+            }
+            else
+            {
+                result[i] = pointB[0] * ((pointA[1] - pointB[1]) / (pointB[0] - pointA[0])) + pointB[1];
+            }
+        }
+        Console.WriteLine("Точка пересечения двух прямых -> [" + String.Join(", ", result) + "]");
+    }
+    
+}
+SetFunctionNumber(pointA);
+SetFunctionNumber(pointB);
+LineIntersectionCoordinates(pointA, pointB);

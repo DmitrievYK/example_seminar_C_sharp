6c8b9d1eaccc2cb38d1f484b8a348d2600524467
DmitrievYK <fireworcks@gmail.com>
2023-09-03 13:11:27 +0300

    Лекция номер 5- как нужно писать код

---
+++ lec3c001/Program.cs
@@ -0,0 +1,4 @@
+﻿// Как нужно писать код 
+// https://learn.microsoft.com/en-us/dotnet/standard/design-guidelines/  ссылка на статью каких правил мы придерживаемся при написании в языке С#
+// Ключевые слова C# -> https://learn.microsoft.com/ru-ru/dotnet/csharp/language-reference/keywords/
+// Лексическая структура (swift) -> https://docs.swift.org/swift-book/documentation/the-swift-programming-language/lexicalstructure/

c3e78b86a9970c7d780b9fab6360bae2b20bc58e
DmitrievYK <fireworcks@gmail.com>
2023-09-13 20:28:05 +0300

    ДЗ семинара №9

---
+++ 8HomeWork002/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+int m = 1;
+int n = 15;
+void PrintSumNumbers(int m, int n)
+{
+    int result = SumNumberBetweenPoints(m, n);
+    Console.WriteLine($"M = {m}; N = {n} -> {result}");
+}
+
+int SumNumberBetweenPoints(int m, int n)
+{
+    if ( m == n) return m;    
+    return m + SumNumberBetweenPoints(m + 1, n);
+    
+}
+PrintSumNumbers(m, n);
---
+++ 8HomeWork003/Program.cs
@@ -0,0 +1,30 @@
+﻿// https://ru.hexlet.io/challenges/intro_to_programming_ackermann_exercise#:~:text=%D0%A4%D1%83%D0%BD%D0%BA%D1%86%D0%B8%D1%8F%20%D0%90%D0%BA%D0%BA%D0%B5%D1%80%D0%BC%D0%B0%D0%BD%D0%B0%20%E2%80%94%20%D0%BF%D1%80%D0%BE%D1%81%D1%82%D0%BE%D0%B9%20%D0%BF%D1%80%D0%B8%D0%BC%D0%B5%D1%80%20%D0%B2%D1%8B%D1%87%D0%B8%D1%81%D0%BB%D0%B8%D0%BC%D0%BE%D0%B9,%D0%BF%D0%B0%D1%80%D0%B0%D0%BC%D0%B5%D1%82%D1%80%D0%BE%D0%B2%20%D0%B8%20%D0%B2%D0%BE%D0%B7%D0%B2%D1%80%D0%B0%D1%89%D0%B0%D0%B5%D1%82%20%D0%BD%D0%B0%D1%82%D1%83%D1%80%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D0%B5%20%D1%87%D0%B8%D1%81%D0%BB%D0%BE.
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int m = 3;
+int n = 2;
+void PrintAckermannFunction(int m, int n)
+{
+    int result = AckermannFunction(m, n);
+    Console.WriteLine($"Результат функции Аккермана для m = {m} и n = {n}: {result}");
+}
+
+int AckermannFunction(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return AckermannFunction(m - 1, 1);
+    }
+    else
+    {
+        return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+    }
+}
+PrintAckermannFunction(m, n);
